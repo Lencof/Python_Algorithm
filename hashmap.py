d64ff188fa01f7d50d50b1ceeb5bd4a1c416d39d
Lencof <71154457+Lencof@users.noreply.github.com>
2021-08-15 12:54:16 +0300

    Update hashmap.py

--- hashmap.py
+++ hashmap.py
@@ -1,1 +1,36 @@
+# __Author__ __Lencof__
+# hashmap.py
 
+class HashMap:
+    def __init__(self):
+        self.size = 6
+        self.map = [None] * self.size
+
+    def _get_hash(self, key):
+        hash = 0
+        for char in str(key):
+            hash += ord(char)
+        return hash % self.size
+
+    def add(self, key, value):
+        key_hash = self._get_hash(key)
+        key_value = [key, value]
+
+        if self.map[key_hash] is None:
+            self.map[key_hash] = list([key_value])
+            return None
+        else:
+            for pair in self.map[key_hash]:
+                if pair[0] == key:
+                    pair[1] = value
+                    return None
+            self.map[key_hash].append(key_value)
+            return True
+
+    def get(self, key):
+        key_hash = self._get_hash(key)
+        if self.map[key_hash] is not None:
+            for pair in self.map[key_hash]:
+                if pair[0] == key:
+                    return pair[1]
+        return None
