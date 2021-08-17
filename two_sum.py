864c45d8b12088ec575e76452aeeec23e048d726
Lencof <71154457+Lencof@users.noreply.github.com>
2021-08-17 15:17:48 +0300

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,1 +1,17 @@
+# __Author__ __Lencof__
+# two_sum.py
 
+# Solution 1
+
+def two_sum(nums, target):
+    # check that the list is not empty
+    if not nums:
+        return None
+    else:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return i,j
+
+print(two_sum([1, 7, 8, 2, 5], 12))
+# (1, 4)
