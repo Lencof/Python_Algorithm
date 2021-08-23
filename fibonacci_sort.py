cd835f04fc301f7d8893a4af6e25cf130eaf6021
Lencof <71154457+Lencof@users.noreply.github.com>
2021-08-23 12:13:43 +0300

    Create fibonacci_sort.py

---
+++ fibonacci_sort.py
@@ -0,0 +1,14 @@
+# __Author__ __Lencof__
+# fibonacci_sort.py
+
+f1 = f2 = 1
+n = 10
+
+print(f1, end=' ')
+print(f2, end=' ')
+
+for i in range(2, n):
+    f1, f2 = f2, f1 + f2
+    print(f2, end=' ')
+    
+# 1 1 2 3 5 8 13 21 34 55
