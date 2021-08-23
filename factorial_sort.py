0c3c7a1016ddc90269b188e9273e99f1580ff5d2
Lencof <71154457+Lencof@users.noreply.github.com>
2021-08-23 12:10:08 +0300

    Update factorial_sort.py

--- factorial_sort.py
+++ factorial_sort.py
@@ -1,1 +1,14 @@
+# __Author__ __Lencof__
+# factorial_sort.py
 
+number = 5
+
+def factorial(n):
+    if n == 0:
+        return 1
+
+    return factorial(n-1) * n
+
+print("Factorial number {n} equals {f}".format(n=number, f =factorial(number)))
+
+# Factorial number 5 equals 120
